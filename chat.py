8238f7148b09ed682688c7c0b5a2270243727b12
mapoetto <davide.sorrentino-cyberhack@unina.it>
2020-03-31 22:46:14 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,11 +1,9 @@
 import socket
-import msvcrt
-import time
 import sys
 
 def being_client():
 
-	listCharacters = []
+	shouldIask = False
 
 	host = input("Insert host (blank for default localhost): ")
 	port = int(input("Insert port: "))
@@ -18,26 +16,23 @@ def being_client():
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.connect((host, port))
 		print("YEAH! we got the connection!!")
+		print("We are client-side, so we will wait until to get a question")
 		while True:
-
-			if msvcrt.kbhit():
+			if shouldIask:
 				question = input("Write a question: ")
 				s.sendall(bytes(question, 'utf-8'))
+				shouldIask = False
 			else:
-				s.sendall(bytes("NOP", 'utf-8'))
-
-			data = s.recv(1024)
-			answ = str(repr(data))
-			if answ == "b'NOP'":
-				pass #NO OPERATION
-			else:
+				data = s.recv(1024)
+				answ = str(repr(data))
 				print("Answer: ", answ[1:]) #elimina la b iniziale messa da python, ovviamente ci sarebbero 3mila controlli da fare prima di questa operazione
+				shouldIask = True
 
 
 
 def being_server():
 
-	listCharacters = []
+	shouldIask = True
 
 	port = int(input("Insert port for hosting: "))
 
@@ -51,23 +46,20 @@ def being_server():
 		with conn:
 			print('YEAH! we got the connection with my friend: ', addr)
 			while True:
-				if msvcrt.kbhit():
+				if shouldIask:
 					question = input("Write a question: ")
 					conn.sendall(bytes(question, 'utf-8'))
+					shouldIask = False
 				else:
-					conn.sendall(bytes("NOP", 'utf-8'))
-
-				data = conn.recv(1024)
-				answ = str(repr(data))
-				if answ == "b'NOP'":
-					pass #NO OPERATION
-				else:
+					data = conn.recv(1024)
+					answ = str(repr(data))
 					print("Answer: ", answ[1:]) #elimina la b iniziale messa da python, ovviamente ci sarebbero 3mila controlli da fare prima di questa operazione
+					shouldIask = True
 
 
 print("Welcome on my TCP based chat")
-print("This chat is NON-BLOCKING... or at least it seems to be :P ")
-role = int(input("Do u wanna be a server or a client? Reply: 1 for server, 2 for client: "))
+print("This chat is only for very educated people.... (: if you already asked a question, pls be kind and wait for the answer!!")
+role = int(input("Do u wanna be a server or a client? (Server will ask first). Reply: 1 for server, 2 for client: "))
 
 if role == 1:
 	being_server()
@@ -75,3 +67,5 @@ elif role == 2:
 	being_client()
 else:
 	print("unknown reply")
+
+			
